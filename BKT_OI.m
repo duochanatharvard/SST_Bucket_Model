50e778084145509f9dc58fd8372ccb18ae24040c
duochanatharvard <duochan@g.harvard.edu>
2018-05-09 16:47:45 -0400

    Add input/output directories

---
+++ BKT_OI.m
@@ -0,0 +1,14 @@
+function dir = BKT_OI(input)
+
+    if strcmp(input,'load_ERA'),
+        dir = '/Volumes/My Passport Pro/ICOADS_3.0/';
+        
+    elseif strcmp(input,'save_driver')
+        dir = '/Users/zen/Research/Git_Code/Bucket_Model/Driver/';
+        
+    elseif strcmp(input,'load_OI_SST')
+        dir = '/Volumes/My Passport Pro/ICOADS_RE/Step_Miscellaneous_ICOADS_RE/';
+        
+    elseif strcmp(input,'load_ICOADS')
+        dir = '/Users/zen/Research/Git_Code/Bucket_Model/ICOADS/';
+    end 
