cb1c8d5439112f1ffe7eb560c14cad82c5bbd10b
duochanatharvard <duochan@g.harvard.edu>
2018-05-10 11:49:45 -0400

    Add output directory in the OI controller

--- BKT_OI.m
+++ BKT_OI.m
@@ -2,13 +2,17 @@ function dir = BKT_OI(input)
 
     if strcmp(input,'load_ERA'),
         dir = '/Volumes/My Passport Pro/ICOADS_3.0/';
-        
+
     elseif strcmp(input,'save_driver')
         dir = '/Users/zen/Research/Git_Code/Bucket_Model/Driver/';
-        
+
     elseif strcmp(input,'load_OI_SST')
         dir = '/Volumes/My Passport Pro/ICOADS_RE/Step_Miscellaneous_ICOADS_RE/';
-        
+
     elseif strcmp(input,'load_ICOADS')
         dir = '/Users/zen/Research/Git_Code/Bucket_Model/ICOADS/';
-    end 
+
+    elseif strcmp(input,'save_output')
+        dir = '/Users/zen/Research/Git_Code/Bucket_Model/';
+
+    end
